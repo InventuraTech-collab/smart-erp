f17d24e8b47d1ea5d22d82ba9a9e1c95b0c1ba16
sitharahussain58-alt <sitharahussain58@gmail.com>
2026-06-06 20:24:26 +0400

    Update page.tsx

--- smart-erp-fixed/app/dashboard/page.tsx
+++ smart-erp-fixed/app/dashboard/page.tsx
@@ -183,7 +183,8 @@ export default function DashboardPage() {
     <ERPLayout>
       <div className="ph">
         <div><div className="pt">Inventory Dashboard</div><div className="ps">Overview of stock, transactions and alerts</div></div>
-        <Link href="/master/items/new"><button className="btn btn-purple">+ New Item</button></Link>
+        <Link href="/master/items/new"><button onClick={() => router.push('/master/items')}>  >+ New Item</button></Link>
+        
       </div>
 
       {/* STAT CARDS */}
